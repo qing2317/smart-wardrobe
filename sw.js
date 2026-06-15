83e43046e0aee12fa08e781658fa9449dc965014
qing2317 <755219865@qq.com>
2026-06-15 17:09:08 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+﻿const CACHE_NAME = "wardrobe-v1";
+const ASSETS = [
+  "index.html",
+  "styles.css",
+  "app.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      return cached || fetch(e.request);
+    })
+  );
+});
